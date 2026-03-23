4a3072cd3787238e87108033455b7054328d01ab
biaguadanhin <beatriz.8857@colegioprogresso.g12.br>
2026-03-23 10:23:27 -0300

    while

---
+++ estrutura-de-operação.js
@@ -0,0 +1,24 @@
+//while, do while, for
+
+let numero1 = 0
+
+while(tabuada <= 40 ){
+    console.log(numero)
+    numero = numero + 4
+}
+
+console.log("sai do while")
+
+
+//crie um programa que recebe um numero e cria a tabuada desse número
+
+let numero = 6
+let tabuada = 0
+let resultado = 0
+while(tabuada <= numero * 10){
+    console.log(resultado)
+    tabuada = tabuada * 1
+    resultado = tabuada * numero
+
+
+}
